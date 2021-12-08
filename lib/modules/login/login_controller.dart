8aaa998b9f2a40f4cda8d6308e12cb54fc5bc395
Edvaldo Santana <edypaulista152@gmail.com>
2021-12-08 20:18:03 -0300

    add: include TextFormField and ElevatedButton

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class LoginController extends ChangeNotifier implements Disposable {
+  final formKey = GlobalKey<FormState>();
   @override
   void dispose() {
     super.dispose();
--- lib/modules/login/widgets/widget_body.dart
+++ lib/modules/login/widgets/widget_body.dart
@@ -1,4 +1,7 @@
+import 'package:builders/builders.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:test_selecti_dev/modules/login/login_controller.dart';
 
 class WidgetBody extends StatefulWidget {
   const WidgetBody({Key? key}) : super(key: key);
@@ -7,9 +10,37 @@ class WidgetBody extends StatefulWidget {
   _WidgetBodyState createState() => _WidgetBodyState();
 }
 
-class _WidgetBodyState extends State<WidgetBody> {
+class _WidgetBodyState extends ModularState<WidgetBody, LoginController> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer<LoginController>(builder: (context, presenter) {
+      return Form(
+        key: controller.formKey,
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(25),
+            child: ListView(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      label: Text('Login'), hintText: 'Digite seu login'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      label: Text('Password'), hintText: 'Digite seu Password'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(onPressed: () {}, child: Container())
+              ],
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
